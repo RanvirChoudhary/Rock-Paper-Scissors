cf08d4102cca06ba6a627e82cd7d8da42008803d
Ranvir Choudhary <ranvir2002choudhary@gmail.com>
2022-04-22 12:24:47 +0530

    optimized code now it is much easier to implement logic

--- script.js
+++ script.js
@@ -1,62 +1,35 @@
 const BtnRock = document.getElementById("BtnRock");
 const BtnScissors = document.getElementById("BtnScissors");
-const BtnPaper = document.getElementById("BtnPaper")
+const BtnPaper = document.getElementById("BtnPaper");
+const PlayerScore = document.getElementById("Player");
+const ComputerScore = document.getElementById("Computer");
 
-function chooseRandom(){
+let random;
+
+function ChooseRandom(){
+  random = Math.floor(Math.random() * 3 + 1)
+  switch(random){
+    case 1:
+      return "Rock"
+    case 2:
+      return "Scissors"
+    case 3:
+      return "Paper"
+    default:
+      return random
+  }
+}
+
+function AddEventListener(){
   BtnRock.addEventListener("click", e => {
-    let random1 = Math.floor(Math.random() * 3 + 1)
-    // console.log(random)
-    switch(random1){
-      case 1:
-        console.log("Rock");
-        break;
-      case 2:
-        console.log("Scissors");
-        break;
-      case 3:
-        console.log("Paper");
-        break;
-      default:
-        console.log(random1);
-        break;
-    }
+    console.log(ChooseRandom())
   })
   BtnScissors.addEventListener("click", e => {
-    let random1 = Math.floor(Math.random() * 3 + 1)
-    // console.log(random)
-    switch(random1){
-      case 1:
-        console.log("Rock");
-        break;
-      case 2:
-        console.log("Scissors");
-        break;
-      case 3:
-        console.log("Paper");
-        break;
-      default:
-        console.log(random1);
-        break;
-    }
+    console.log(ChooseRandom())
   })
   BtnPaper.addEventListener("click", e => {
-    let random1 = Math.floor(Math.random() * 3 + 1)
-    // console.log(random)
-    switch(random1){
-      case 1:
-        console.log("Rock");
-        break;
-      case 2:
-        console.log("Scissors");
-        break;
-      case 3:
-        console.log("Paper");
-        break;
-      default:
-        console.log(random1);
-        break;
-    }
+    console.log(ChooseRandom())
   })
   
 }
-chooseRandom()
+AddEventListener()
